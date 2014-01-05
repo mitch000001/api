e02c9f01c54fb9e42b65c7f4246e6baebfa6c532
Raphael Randschau <nicolai86@me.com>
2014-01-05 10:09:01 +0100

    blank handlers for timeframes

--- umsatz.go
+++ umsatz.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-  "fmt"
+	// "encoding/json"
+	"github.com/gorilla/mux"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
 )
 
+func TimeframesIndexHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "[]")
+}
+
+func TimeframesCreationHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "[]")
+}
+
 func main() {
-  fmt.Println("hello world")
+	var port string = os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	l, err := net.Listen("tcp", "0.0.0.0:"+port)
+	if nil != err {
+		log.Fatalln(err)
+	}
+	log.Printf("listening on %v", l.Addr())
+
+	r := mux.NewRouter()
+	s := r.PathPrefix("/timeframes").Subrouter()
+	s.HandleFunc("/", TimeframesIndexHandler).
+		Methods("GET")
+	s.HandleFunc("/", TimeframesCreationHandler).
+		Methods("POST")
+
+	http.Handle("/", r)
+	http.Serve(l, r)
 }
